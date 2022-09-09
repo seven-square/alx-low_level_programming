254e1b1c6541764e1139a79f8559f7374a7485be
Success Moranti <successmoranti@gmail.com>
2022-09-08 20:59:26 -0700

    chore: print size of various types on the computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	char a
-	int b
-	long int c 
-	long long int d
-	float e
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
