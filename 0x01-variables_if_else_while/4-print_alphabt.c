a354a217e58388b5f3941319ba5c6f1edd3ea11a
Success Moranti <successmoranti@gmail.com>
2022-09-09 19:30:27 -0700

    chore: print alphabet in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Result: Always (0) Success
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if ((ch != 'q') && (ch != 'e'))
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
