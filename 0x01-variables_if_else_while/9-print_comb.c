860168ae073716b64708e03339f406f70425adb6
Success Moranti <successmoranti@gmail.com>
2022-09-10 03:47:30 -0700

    chore: print all possible combinations of single digit nums

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,13 +11,9 @@ int main(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		if (i == '9')
+		putchar(i);
+		if (i < '9')
 		{
-			putchar(i);
-		}
-		else
-		{
-			putchar(i);
 			putchar(',');
 			putchar(' ');
 		}
