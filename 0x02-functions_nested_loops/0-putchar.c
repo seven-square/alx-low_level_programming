82df875f2797dc17da627aa55d8399f9f891ac3f
Success Moranti <successmoranti@gmail.com>
2022-09-13 08:45:43 -0700

    chore: print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	ch = "_putchar";
+	_putchar(ch);
+	_putchar('\n');
+	return (0);
+}
