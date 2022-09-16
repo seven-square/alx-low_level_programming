7b2c566d087fa82812587219a9e7b5f44cd64ea3
Success Moranti <successmoranti@gmail.com>
2022-09-15 20:16:43 -0700

    chore: write a function that prints 10 times, the numbers from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			_putchar(j + '0');
+			j++;
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
