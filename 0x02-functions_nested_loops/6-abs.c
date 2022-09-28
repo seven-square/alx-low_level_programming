9ed4461042b268a28f6a0521afa82532873c98ec
Success Moranti <successmoranti@gmail.com>
2022-09-28 01:51:55 -0700

    chore: compute absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @i: integer argument of which to compute an absolute value
+ *
+ * Return: absolute value of integer i
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		i *= -1;
+		_putchar(i + 'o');
+	}
+	else
+	{
+		_putchar(i + 'o');
+	}
+}
