42c12c7ac09f6f391f5f238b1c0121f6b7efe612
Success Moranti <successmoranti@gmail.com>
2022-09-28 00:05:03 -0700

    chore: print 10 times the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints all a-z on 10 lines
+ *
+ * Return: void.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 'a';
+		while (j <= 'z')
+		{
+			_putchar(j);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	_putchar('\n');
+}
+
